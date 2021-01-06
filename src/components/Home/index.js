ce10a3b43f498c0399b013c94938eff383c25c2a
Ru <rudikraeher@gmail.com>
2021-01-06 14:30:15 -0800

    font awesome icons

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,6 +3,7 @@ import "../Project/Project.css";
 import { Row, Col } from "antd";
 import ProfilePic from '../ProfilePic/ProfilePic';
 
+
 const Home = () => {
     return (
         <div>
@@ -12,6 +13,11 @@ const Home = () => {
                     <h1>Rudi Kraeher</h1>
                     <h2>Full Stack Web Developer</h2>
                     <h3>HTML/CSS | JavaScript | React | Node</h3>
+                    <div className="icons">
+                        <i className="fab fa-github fa-3x" style={{ margin: "1vw" }}></i>
+                        <i className="fab fa-linkedin fa-3x" style={{ margin: "1vw" }}></i>
+                        <i className="fas fa-envelope fa-3x" style={{ margin: "1vw" }}></i>
+                    </div>
                 </Col>
                 <ProfilePic />
             </Row>
