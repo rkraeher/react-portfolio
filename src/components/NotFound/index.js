fb430e06300cfe9019a5d39d48251f470014bb0d
Ru <rudikraeher@gmail.com>
2021-03-04 16:04:19 -0800

    Not found component font size and position

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 const NotFound = () => {
     return (
         <div>
-            <h1>404 - Not Found!</h1>
-            <Link to="/">
+            <h1 style={{ justifyContent: "center", display: "flex", fontSize: "4em" }}>404 - Page Not Found!</h1>
+            <Link to="/" style={{ justifyContent: "center", display: "flex", fontSize: "2em" }} >
                 Go Home
             </Link>
         </div>
